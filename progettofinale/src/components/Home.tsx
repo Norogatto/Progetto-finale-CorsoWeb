dd113c214427b9a55c656c669404803857c4dbc4
Francesco Ruocco <francyruocco97@gmail.com>
2025-07-28 18:03:21 +0200

    Create Home
    Ho creato la home principale del sito:
    1. descrizione del sito
    2. bottoni

---
+++ progettofinale/src/components/Home.tsx
@@ -0,0 +1,26 @@
+
+export default function Home() {
+
+
+  return (
+<div>
+        <div>
+            <h1>Benvenuto in Task Master</h1>
+            <p>Questo sito è sviluppato per organizzare al meglio le tue task</p>
+        </div>
+
+
+        <div>
+            <input type="button">Accedi come utente</input>
+        </div>
+        <div>
+            <input type="button">Accedi come amministratore</input>
+        </div>
+        <div>
+            <input type="button">Accedi come ospite</input>
+        </div>
+
+</div>
+  )
+}
+
